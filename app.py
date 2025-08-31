a684c9ee825f5c9df285b536f75bd4639599e99b
wilo3161 <119455449+wilo3161@users.noreply.github.com>
2025-08-31 00:02:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ st.set_page_config(
 st.markdown("""
 <style>
     .main { background-color: dimgray; }
-    .stApp { background-color: black; }
+    .stApp { background-color: dimgray; }
     .kpi-card {
         background: darkturquoise;
         border-radius: 12px;
